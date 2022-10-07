e9735ec0dced7aceb5ac127b4492ac0c4f1a35bf
hanakoori <paola.espinoza.cascante@est.una.ac.cr>
2022-10-07 09:32:55 -0600

    fix(src): add default mode dark

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -61,6 +61,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: "dark",
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
       navbar: {
         logo: {
           alt: "Tikuna Logo",

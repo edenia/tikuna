e37597ce61a3d148b598a98e2e5a0840e9757de8
edenia <edenia@users.noreply.github.com>
2022-10-07 21:47:40 +0000

    Deploy website - based on 3b7f23c279c614e517fc4d10415c6300272256ff

--- assets/js/814f3328.6a7fc21d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktikuna=self.webpackChunktikuna||[]).push([[535],{5641:t=>{t.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"First Blog Post","permalink":"/blog/first-blog-post"}]}')}}]);
--- es/assets/js/814f3328.7e5b94f5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktikuna=self.webpackChunktikuna||[]).push([[535],{5641:t=>{t.exports=JSON.parse('{"title":"Post recientes","items":[{"title":"First Blog Post","permalink":"/es/blog/first-blog-post"}]}')}}]);

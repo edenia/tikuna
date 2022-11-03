a84e4b2afc76232c906c07dd80783f30cb4aec46
root <root@Hanakori.localdomain>
2022-11-03 00:07:24 -0600

    feat(src): add new structure to hero component

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,45 +1,47 @@
 import React from "react";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Translate, { translate } from "@docusaurus/Translate";
+import Translate from "@docusaurus/Translate";
 import Layout from "@theme/Layout";
 import Features from "@site/src/components/Features";
 
 import styles from "./index.module.css";
 
 const HomepageHero = () => {
-  const { siteConfig } = useDocusaurusContext();
 
   return (
     <header className={styles.heroBanner}>
-      <div className={clsx("container", styles.container)}>
-        <div className={styles.image}></div>
-        <h1 className={styles.subtitle}>
-          <Translate>
-            An AI-based Blockchain security monitoring system.
-          </Translate>
-        </h1>
-        <div className={styles.buttons}>
-          <Link
-            className={clsx("button--lg", styles.buttonText)}
-            to="/docs/research/intro"
-          >
-            <Translate>Project Intro</Translate>
-          </Link>
+      <div className={styles.content}>
+        <div className={clsx("container", styles.container, )}>
+          <div className={styles.image}></div>
+          <h1 className={styles.subtitle}>
+            <Translate>
+              An AI-based Blockchain security monitoring system.
+            </Translate>
+          </h1>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx("button--lg", styles.buttonText)}
+              to="/docs/research/intro"
+            >
+              <Translate>Project Intro</Translate>
+            </Link>
+          </div>
         </div>
+      </div>
+      <div className={clsx("container", styles.diagramContainer, )}>
         <div className={styles.subtitleDiagram}>
-        <Translate>
+          <Translate>
             Here is how it works:
           </Translate>
         </div>
+        <div className={styles.diagram}></div>
       </div>
     </header>
   );
 };
 
 const Home = () => {
-  const { siteConfig } = useDocusaurusContext();
 
   return (
     <Layout description="">

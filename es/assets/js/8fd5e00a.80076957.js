03a9c1b7fad0fc63ee316b1b416a929b23d72bfc
edenia <edenia@users.noreply.github.com>
2023-08-03 20:41:50 +0000

    Deploy website - based on 434f45d8c103b89121a04b4ccf7085224e96057b

---
+++ es/assets/js/8fd5e00a.80076957.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktikuna=self.webpackChunktikuna||[]).push([[568],{4510:e=>{e.exports=JSON.parse('{"permalink":"/es/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
